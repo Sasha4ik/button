352071487d1b74cf126d5626d8605f90d61b8c04
Sasha <165439653+Sasha4ik@users.noreply.github.com>
2024-06-17 13:45:28 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let input = document.getElementById('inp')
+let button = document.getElementById('but')
+let block = document.getElementById('div1')
+// console.log(input, button);
+
+let func = () => {
+    let text = input.value
+    if(text) block.innerHTML = text
+}
+
+button.onclick = func
